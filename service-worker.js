f023f0b1ae5302f8057120d1839e397ad625d50c
leonardoGOMX <leonardolool86@gmail.com>
2025-05-20 12:54:32 -0600

    first commit

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'facturagi-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './bootstrap/bootstrap-5.3.6-dist/css/bootstrap.min.css',
+  './bootstrap/bootstrap-5.3.6-dist/js/bootstrap.bundle.min.js',
+  './css/globaleStyle.css',
+  './images/Facturagi.png',
+  './icons/25231.png',
+  './icons/Facturagi.png',
+  './manifest.json'
+];
+
+// Instala el service worker y guarda archivos en caché
+self.addEventListener('install', function (event) {
+  console.log('[Service Worker] Instalando y cacheando archivos...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Activa el service worker y limpia cachés viejas si existen
+self.addEventListener('activate', function (event) {
+  console.log('[Service Worker] Activado');
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Intercepta peticiones y sirve desde caché si no hay internet
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        return response || fetch(event.request);
+      }).catch(() => {
+        return caches.match('./index.html');
+      })
+  );
+});
